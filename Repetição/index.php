5459673cfde6c19c48761a91c30baa29efbbb006
jota061 <jpgoldschmidt1602@gmail.com>
2025-05-23 09:00:31 -0300

    Add files via upload

---
+++ Repetição/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Repetição</title>
+</head>
+<body>
+<?php
+        // Comando de Repetição - FOR
+        //Escreva os números de 1 a 10 - PAPA
+        echo "<h1>Escreva de 1 a 10</h1>";
+        for($i=1;$i<=10;$i++){
+            echo "$i <br>";
+        }
+        ?>
+</body>
+</html>
